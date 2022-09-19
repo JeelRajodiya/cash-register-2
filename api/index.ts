0f7ce9caae59dd7dc314997b0594ab64956a4208
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-09-19 21:28:23 +0530

    testing

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,4 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import DB from "./util/database";
 export default async (request: VercelRequest, response: VercelResponse) => {
-	const r = await DB.users.find({}).toArray();
-
-	response.send(r);
+	response.send(200);
 };
