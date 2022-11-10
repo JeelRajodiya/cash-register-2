ea9d365f4b2ca405c0cda92add896ed95320242d
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2022-11-10 21:17:41 +0530

    search page in progress

--- pages/Search.tsx
+++ pages/Search.tsx
@@ -1,9 +1,25 @@
 import Layout from "../components/Layout";
-
+import styles from "../styles/Search.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button } from "@mui/material";
 export default function Search() {
 	return (
 		<Layout>
-			<div style={{ height: "100%" }}>Search</div>
+			<div className={styles.searchWindow}>
+				<div className={styles.searchBar}>
+					<input
+						className={styles.mainInput}
+						placeholder="Start Searching ..."
+					></input>
+					<Button
+						color="inherit"
+						// sx={{ borderRadius: "2em" }}
+						className={styles.searchBtn}
+					>
+						<SearchIcon />
+					</Button>
+				</div>
+			</div>
 		</Layout>
 	);
 }
