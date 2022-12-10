1dfbe3593a104ff20500628a6c07c8d3b8018005
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2022-12-10 17:29:08 +0530

    messed up

--- pages/Graph.tsx
+++ pages/Graph.tsx
@@ -3,10 +3,9 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
 import { getCookie } from "cookies-next";
-const ReactApexChart = dynamic(() => import("react-apexcharts"), {
-	ssr: false,
-});
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 
+dynamic(() => import("recharts").then(Line), { ssr: false });
 async function fetchGraphData(
 	setData: (value: any) => void,
 	filterType: string,
@@ -19,86 +18,32 @@ async function fetchGraphData(
 	const data: any[] = await response.json();
 	let graphData: any[] = [];
 	// console.log(data);
-	for (let i of Object.keys(data)) {
+	let i: string;
+	for (i of Object.keys(data)) {
 		graphData.push({
 			x: i,
 			y: data[i],
 		});
 	}
-
-	setData((x: any) => {
-		const newData = {
-			...x,
-			series: [
-				{
-					name: "Series 1",
-					data: graphData,
-				},
-			],
-		};
-		// console.log(newData.series.data.length);
-		return newData;
-	});
-	// console.log(data);
+	console.log(graphData);
+	setData(graphData);
 }
 
 export default function Search() {
 	const [chartData, setChartData] = useState<any>();
 	useEffect(() => {
-		fetchGraphData(setChartProps, "month", "2021-08");
+		fetchGraphData(setChartData, "month", "2021-08");
 	}, []);
-	const [chartProps, setChartProps] = useState({
-		options: {
-			colors: ["#00BAEC"],
-			stroke: {
-				width: 1,
-			},
-			dataLabels: {
-				enabled: false,
-			},
-			fill: {
-				gradient: {
-					enabled: true,
-					opacityFrom: 0.55,
-					opacityTo: 0,
-				},
-			},
-			markers: {
-				size: 5,
-				colors: ["#000524"],
-				strokeColor: "#00BAEC",
-				strokeWidth: 3,
-			},
-
-			tooltip: {
-				theme: "dark",
-			},
-			// xaxis: {
-			// 	type: "datetime",
-			// },
-			// yaxis: {
-			// 	min: 0,
-			// 	tickAmount: 4,
-			// },
-		},
-		series: [
-			{
-				name: "Series 1",
-				data: [45, 52, 38, 45, 19, 23, 2],
-			},
-		],
-	});
 
 	return (
 		<Layout>
 			<div style={{ height: "100%" }}>
-				<ReactApexChart
-					options={chartProps.options}
-					series={chartProps.series}
-					type="area"
-					height={350}
-					width={500}
-				/>
+				<LineChart width={600} height={300} data={chartData}>
+					<Line type="monotone" dataKey="uv" stroke="#8884d8" />
+					<CartesianGrid stroke="#ccc" />
+					<XAxis dataKey="name" />
+					<YAxis />
+				</LineChart>
 			</div>
 		</Layout>
 	);
