45af0e26d526f7b422a22555d69af2764b4c7975
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2022-12-24 23:04:13 +0530

    Revert "button overlapping fixed"
    This reverts commit fe4b7329642ac6810dd77a4da176de1a136bfb56.

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -45,7 +45,6 @@ export default function Entry(props: EntryProps2) {
 			<Button
 				color="error"
 				size="large"
-				sx={{ zIndex: -1 }}
 				onClick={() => {
 					deleteEntry(props.entryID);
 					props.setDataUpdateHash(new Date().getTime());
